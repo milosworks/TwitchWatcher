8448b3d2b17756c1f76a36ba5e4bd7983fa64d9a
VyrekXD <vyrekxd@gmail.com>
2021-08-10 18:44:27 -0500

    Fix debug x5

--- src/Util/debug.js
+++ src/Util/debug.js
@@ -19,14 +19,11 @@ export async function Debug(debugText, Page, name) {
 
 		await Page.screenshot({
 			path: join(
-				path,
-				new Date()
+				path`${new Date()
 					.toLocaleDateString('en-US')
 					.split('')
 					.map((x) => x.replace('/', '-'))
-					.join(''),
-				'-',
-				`${name}.png`
+					.join('')}-${name}.png`
 			)
 		})
 	}
